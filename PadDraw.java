5b6e968b49d3637a973e98b0b4ae125a68dff1b2
dahrapucher <dahra.pucher@gmail.com>
2017-05-23 14:28:26 -0700

    worked on drawing

---
+++ PadDraw.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class PadDraw extends JComponent
+{
+	Graphics2D graph2D;
+	Image i;
+	int firstX, firstY, secondX, secondY;
+	
+	public PadDraw()
+	{
+		setDoubleBuffered(false);
+		addMouseListener(new MouseAdapter()
+		{
+			public void mousePressed(MouseEvent a)
+			{
+				secondX = a.getX();
+				secondX = a.getY();
+			}
+		});
+		
+		addMouseMotionListener(new MouseMotionAdapter()
+		{
+			public void mouseDragged(MouseEvent a)
+			{
+				firstX = a.getX();
+				firstY = a.getY();
+				if(graph2D != null)
+				{
+					graph2D.drawLine(secondX, secondY, firstX, firstY);
+					repaint();
+					secondX = firstX;
+					secondY = firstY;
+				}
+				
+				
+			
+				
+				// secondX = firstY;
+				// secondY = firstX;
+				// firstX= secondX;
+				// firstY=secondY;
+			}
+		});
+	}
+	
+	public void paintComponent(Graphics a)
+	{
+		if(i == null)
+		{
+			i = createImage(getSize().width, getSize().height);
+			graph2D = (Graphics2D)i.getGraphics();
+			graph2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			clear();
+		}
+		a.drawImage(i, 0, 0, null);
+	}
+	
+	public void clear()
+	{
+		graph2D.setPaint(Color.white);
+		graph2D.fillRect(0, 0, getSize().width, getSize().height);
+		graph2D.setPaint(Color.black);
+		repaint();
+	}
+}
---
+++ Paint.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Paint
+{
+	public static void main(String[]args)
+	{
+		JFrame frame = new JFrame("Paint Program");
+		//drawing
+		Container content = frame.getContentPane();
+		content.setLayout(new BorderLayout());
+		final PadDraw drawPad = new PadDraw();
+		content.add(drawPad, BorderLayout.CENTER);
+		
+		frame.setSize(1000,1000);
+		frame.setLocation(100,100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		// panel.setPreferredSize(new Dimension(32,68));
+		// panel.setMinimumSize(new Dimension(32,68));
+		// panel.setMaximumSize(new Dimension(32,68));
+		panel.setBackground(Color.white);
+		frame.add(panel);
+		
+		//buttons
+		Dimension d = new Dimension(100,80);
+		
+		JButton buttonOne = new JButton();
+		buttonOne.setText("RED");
+		buttonOne.setBackground(Color.red);
+		buttonOne.setPreferredSize(d);
+		
+		JButton buttonTwo = new JButton();
+		buttonTwo.setText("YELLOW");
+		buttonTwo.setBackground(Color.yellow);
+		buttonTwo.setPreferredSize(d);
+		
+		JButton buttonThree = new JButton();
+		buttonThree.setText("ORANGE");
+		buttonThree.setBackground(Color.orange);
+		buttonThree.setPreferredSize(d);
+		
+		JButton buttonFour = new JButton();
+		buttonFour.setText("GREEN");
+		buttonFour.setBackground(Color.green);
+		buttonFour.setPreferredSize(d);
+		
+		JButton buttonFive = new JButton();
+		buttonFive.setText("BLUE");
+		Color myBlue = new Color(30,144,255);
+		buttonFive.setBackground(myBlue);
+		buttonFive.setPreferredSize(d);
+		
+		panel.add(buttonOne);
+		panel.add(buttonThree);
+		panel.add(buttonTwo);
+		panel.add(buttonFour);
+		panel.add(buttonFive);
+	}
+}
+
