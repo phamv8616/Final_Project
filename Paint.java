18047f6e9dcd549694051580f6f3087ffcd5a763
dahrapucher <dahra.pucher@gmail.com>
2017-05-30 14:05:37 -0700

    created split pane

--- Paint.java
+++ Paint.java
@@ -2,55 +2,65 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 public class Paint
 {
 	public static void main(String[]args)
 	{
 		JFrame frame = new JFrame("Paint Program");
-		//drawing
-		Container content = frame.getContentPane();
-		content.setLayout(new BorderLayout());
-		Draw drawPad = new Draw();
-		content.add(drawPad);
+	
+		Container container = frame.getContentPane();
+		// content.setLayout(new BorderLayout());
+		Draw drawing = new Draw();
+		container.add(drawing);
 		
-		frame.setSize(800,500);
+		frame.setSize(700,750);
 		// frame.setLocation(100,100);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
 		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout());
+		// panel1.setBounds(100, 100, 100, 140);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		// JPanel panel2 = new JPanel();
+		
+		// panel1.setLayout(new FlowLayout());
+		// panel2.setLayout(new FlowLayout());
 		// panel.setPreferredSize(new Dimension(32,68));
 		// panel.setMinimumSize(new Dimension(32,68));
 		// panel.setMaximumSize(new Dimension(32,68));
 		panel.setBackground(Color.white);
 		
 		//buttons
-		Dimension d = new Dimension(100,80);
+		Dimension d = new Dimension(150,200);
 		
 		JButton buttonOne = new JButton();
-		buttonOne.setText("RED");
+		buttonOne.setText("          RED            ");
 		buttonOne.setBackground(Color.red);
 		buttonOne.setPreferredSize(d);
 		
 		JButton buttonTwo = new JButton();
-		buttonTwo.setText("YELLOW");
+		buttonTwo.setText("       YELLOW       ");
 		buttonTwo.setBackground(Color.yellow);
 		buttonTwo.setPreferredSize(d);
 		
 		JButton buttonThree = new JButton();
-		buttonThree.setText("ORANGE");
+		buttonThree.setText("       ORANGE       ");
 		buttonThree.setBackground(Color.orange);
 		buttonThree.setPreferredSize(d);
 		
 		JButton buttonFour = new JButton();
-		buttonFour.setText("GREEN");
+		buttonFour.setText("        GREEN          ");
 		buttonFour.setBackground(Color.green);
 		buttonFour.setPreferredSize(d);
 		
 		JButton buttonFive = new JButton();
-		buttonFive.setText("BLUE");
+		buttonFive.setText("          BLUE           ");
 		Color myBlue = new Color(30,144,255);
 		buttonFive.setBackground(myBlue);
 		buttonFive.setPreferredSize(d);
@@ -60,6 +70,12 @@ public class Paint
 		panel.add(buttonTwo);
 		panel.add(buttonFour);
 		panel.add(buttonFive);
+		
+		// panel2.add(drawing);
+		
+		// JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, panel2, panel1);
+		// splitPane.setDividerLocation(10);
+		
 		frame.add(panel);
 	}
 }
